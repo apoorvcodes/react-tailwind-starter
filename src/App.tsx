2d4afc7f91cf0ca88052a25290d6c5aef31e8063
Apoorv <tofu.develops@gmail.com>
2021-09-04 10:49:36 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-black"> 
+    <h1 className="flex justify-center text-6xl pt-12 text-purple-200">Tailwind Css up and running</h1> </div>
+  );
+}
+
+export default App;
